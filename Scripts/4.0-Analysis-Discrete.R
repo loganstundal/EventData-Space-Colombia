75f67bfd213957aab6e68a3dcfc0de7144b44348
Logan Stundal <logan.stundal@gmail.com>
2021-02-28 19:41:23 -0600

    Cleaned up 4.0-Analysis-Discrete
    Moved code to translate sp. probit models into a table for email or pulication into a standalone function.

--- Scripts/4.0-Analysis-Discrete.R
+++ Scripts/4.0-Analysis-Discrete.R
@@ -43,7 +43,7 @@ load("data/data_spatial.RData")
 # Functions
 #---------------------------#
 source("Scripts/Functions/se_robust.R")
-
+source("c:/users/logan/documents/r/r - simple-functions/reg_tbl.r")
 
 
 #-----------------------------------------------------------------------------#
@@ -53,16 +53,22 @@ source("Scripts/Functions/se_robust.R")
 fn  <- paste0("distance_bogota_km_ln + I(distance_bogota_km_ln^2) + ",
               "pop_sum_ln + I(pop_sum_ln^2) +",
               "area_km2_ln + I(area_km2_ln^2) +" ,
+              # "bogota_dummy + "                     # This is producing some explosive standard errors. Assuming major optimizer problems for second deriv being masked with inclusion of this.
               # "log(terrain_ri_mean_m) + ",          # better for observed mods
               "terrain_ri_mean_m + ",                 # better for under mods (nonlinear distance emerges for icews)
-              "forest_per + nl_mean + bogota_dummy")
+              "forest_per + nl_mean")
 dvs <- c("icews_bin", "ged_bin", "cinep_bin",
          "icews_cinep_under","ged_cinep_under")
 
 fs  <- lapply(paste(dvs, fn, sep = " ~ "), as.formula)
 names(fs) <- dvs
 
-rm(dvs, fn)
+
+varnames <- str_split(fn, "\\+") %>% unlist
+varnames <- lapply(varnames, function(x){
+  tmp = str_trim(x)
+  str_remove_all(tmp, pattern = "I|[()]")
+}) %>% unlist
 #-----------------------------------------------------------------------------#
 
 
@@ -82,109 +88,74 @@ ns_probits <- lapply(fs, function(x){
       data    = colombia_cs)
 })
 
-ns_p_ses <- lapply(ns_probits, function(x){
-  sqrt(diag(sandwich::vcovHC(x, type = "HC1")))
-})
+texreg::screenreg(ns_probits)
 
-ns_p_pvals <- lapply(1:length(fs), function(x){
-  2 * (1 - pnorm(abs(ns_probits[[x]]$coefficients)/ns_p_ses[[x]]))
-})
-
-texreg::htmlreg(l = ns_probits,
-                override.se = ns_p_ses,
-                override.pvalues = ns_p_pvals,
-                file = "Documents/tst.html")
+reg_tbl(model_list     = ns_probits,
+        model_names    = dvs,
+        variable_names = varnames)
 # ----------------------------------- #
-
-
-
-
-
-
-
-
-
-
 
 
 # ----------------------------------- #
 # Years-Grouped
 # ----------------------------------- #
-try(suppressWarnings({rm(vanilla, vses, vpvl)}))
-
-yrs <- unique(colombia_yg$year_grouped)
-
-vanilla <- lapply(yrs, function(y){
-  tmp <- colombia_yg %>% filter(year_grouped == y)
-  lapply(fs, function(x){
-    glm(formula = x,
-        family  = binomial(link = "probit"),
-        data    = tmp)
-  })
-})
-
-names(vanilla) <- paste0("Year_",yrs)
-
-# vses <- lapply(vanilla, function(y){
-#   lapply(y, function(x){
-#     sqrt(diag(sandwich::vcovHC(x, type = "HC1")))
-#   })
-# })
-#
-# vpval <- lapply(vanilla, function(y){
-#   lapply(1:length(fs), function(x){
-#     2 * (1 - pnorm(abs(y[[x]]$coefficients)/vses[[x]][1]))
-#   })
-# })
-
-
-cat("\14");texreg::screenreg(l = vanilla$`Year_2002-03`)
-cat("\14");texreg::screenreg(l = vanilla$`Year_2004-05`)
-cat("\14");texreg::screenreg(l = vanilla$`Year_2006-07`)
-cat("\14");texreg::screenreg(l = vanilla$`Year_2008-09`)
-
-
-
-rm(vanilla, vses, vpvl)
-
+      # try(suppressWarnings({rm(vanilla, vses, vpvl)}))
+      #
+      # yrs <- unique(colombia_yg$year_grouped)
+      #
+      # vanilla <- lapply(yrs, function(y){
+      #   tmp <- colombia_yg %>% filter(year_grouped == y)
+      #   lapply(fs, function(x){
+      #     glm(formula = x,
+      #         family  = binomial(link = "probit"),
+      #         data    = tmp)
+      #   })
+      # })
+      #
+      # names(vanilla) <- paste0("Year_",yrs)
+      #
+      # # vses <- lapply(vanilla, function(y){
+      # #   lapply(y, function(x){
+      # #     sqrt(diag(sandwich::vcovHC(x, type = "HC1")))
+      # #   })
+      # # })
+      # #
+      # # vpval <- lapply(vanilla, function(y){
+      # #   lapply(1:length(fs), function(x){
+      # #     2 * (1 - pnorm(abs(y[[x]]$coefficients)/vses[[x]][1]))
+      # #   })
+      # # })
+      #
+      #
+      # cat("\14");texreg::screenreg(l = vanilla$`Year_2002-03`)
+      # cat("\14");texreg::screenreg(l = vanilla$`Year_2004-05`)
+      # cat("\14");texreg::screenreg(l = vanilla$`Year_2006-07`)
+      # cat("\14");texreg::screenreg(l = vanilla$`Year_2008-09`)
+      #
+      #
+      #
+      # rm(vanilla, vses, vpvl)
 # ----------------------------------- #
-
-
-
-
-
-
-
-
-
-
-
-
-
+#-----------------------------------------------------------------------------#
 
 
 
 #-----------------------------------------------------------------------------#
+# SPATIAL PROBITS
+#-----------------------------------------------------------------------------#
 
-basic <- formula(icews_bin ~ distance_bogota_km_ln + pop_sum_ln + terrain_ri_mean_m)
-# Note - Bogota dummy is fine. Replace basic in m1 below to see
-#        change in rho.
-
-# This drives me crazy , M&G definitely swapped lambda and rho. rho is the spatial lag
-# while lambda is spatial error param... typically.
-
-# Then, once we filter out spatial error dependence using proper functional
-# form, we can accurately estimate a spatial lag model.
-
+# ----------------------------------- #
+# Spatial error probits (SEM)
+# ----------------------------------- #
 m1  <- SpatialProbitFit(formula = fs$icews_bin,
                         data    = colombia_cs,
                         W       = nb.queen.mat,
-                        DGP     = 'SAR',
+                        DGP     = 'SEM',
                         method  = "conditional",
                         varcov  = 'varcov',
                         control = list(iW_CL = 6))
 
-m2  <- SpatialProbitFit(formula = f2,
+m2  <- SpatialProbitFit(formula = fs$ged_bin,
                         data    = colombia_cs,
                         W       = nb.queen.mat,
                         DGP     = 'SEM',
@@ -192,15 +163,13 @@ m2  <- SpatialProbitFit(formula = f2,
                         varcov  = 'varcov',
                         control = list(iW_CL = 6))
 
-m3  <- SpatialProbitFit(formula = f3,
+m3  <- SpatialProbitFit(formula = fs$cinep_bin,
                         data    = colombia_cs,
                         W       = nb.queen.mat,
                         DGP     = 'SEM',
                         method  = "conditional",
                         varcov  = 'varcov',
                         control = list(iW_CL = 6))
-
-
 
 m4  <- SpatialProbitFit(formula = fs$icews_cinep_under,
                         data    = colombia_cs,
@@ -218,50 +187,35 @@ m5  <- SpatialProbitFit(formula = fs$ged_cinep_under,
                         varcov  = 'varcov',
                         control = list(iW_CL = 6))
 
+# Organize models
+sp_probits <- list(m1,m2,m3,m4,m5)
+names(sp_probits) <- dvs
 
+# Spatial probits - parameter extract
+results       <- list()
+results$coefs <- lapply(sp_probits, function(x){c(x@beta, x@rho)})
 
-
-
-
-
-
-
-
-reported <- list("icews" = m1,
-                 "ged"   = m2,
-                 "cinep" = m3)
-results  <- list()
-
-results$coefs <- lapply(reported, function(x){c(x@beta, x@rho)})
-
-results$ses   <- lapply(1:3, function(x){
+results$ses   <- lapply(1:length(dvs), function(x){
   print(sprintf('Working on model: %s',x))
-  se_robust(reported[[x]], robust = TRUE, hc = "HC1")})
+  se_robust(sp_probits[[x]], robust = TRUE, hc = "HC1")})
 
-results$pvals <- lapply(1:3, function(x){
-  2 * (1 - pnorm(abs(results$coefs[[x]])/results$ses[[x]]))})
+# Organize values to markdown table (pandoc pipe format)
+res <- list()
+res[[1]] <- results$coefs
+res[[2]] <- results$ses
 
-results$lliks <- lapply(reported, function(x){c(round(x@loglik,3))})
 
-results$dfs <- lapply(1:3, function(x){
-  data.frame("Coefs" = results$coefs[[x]],
-             "SEs"   = results$ses[[x]],
-             "Pvals" = results$pvals[[x]])
-  })
+reg_tbl(model_data     = res,
+        model_names    = dvs,
+        variable_names = c(varnames, "rho"))
+# ----------------------------------- #
 
-results$dfs
 
-v   <- c("ICEWS","GED","CINEP")
-tst <- data.frame()
 
-for(i in 1:3){
-  tmp <- results$dfs[[i]] %>%
-    rownames_to_column(., var = "variable") %>%
-    dplyr::select(variable, everything())
-  tmp$Model = v[i]
 
-  tst <- bind_rows(tst, tmp)
-}
+
+
+
 
 
 
@@ -280,5 +234,5 @@ for(i in 1:3){
 #-----------------------------------------------------------------------------#
 # SAVE                                                                    ----
 #-----------------------------------------------------------------------------#
-# save.image(file = "models_discrete.RData")
+# save.image(file = "data/models_discrete.RData")
 #rm(list = ls())
